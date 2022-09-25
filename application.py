0b9aef819549c19e3f3a213f79ff1e3173f3fe38
arpanojha <ojhaarpan12@gmail.com>
2022-09-25 17:20:53 -0400

    Update application.py

--- application.py
+++ application.py
@@ -21,3 +21,65 @@ def hello_world():
 @application.route('/programming_languages/<programming_language_name>')
 def get_programming_language(programming_language_name):
    return in_memory_datastore[programming_language_name]
+
+import requests 
+import base64
+user='clevercart-c4a68e29f9afcc45f2e56902f0151c991072864823612119086'
+passw = '25MY35fFo7Uh7fZJfzdrB17OJ42VC4rnFTd4jhI4'
+code_it = user+':'+passw
+message_bytes = code_it.encode('ascii')
+base64_bytes = base64.b64encode(message_bytes)
+base64_message = base64_bytes.decode('ascii')
+code_it = "Basic "+base64_message
+
+def get_me_the_fuck_in():
+    headers = {"Content-Type":"application/x-www-form-urlencoded","Authorization":code_it}
+    data = {"grant_type":"client_credentials","scope":"product.compact"}
+    
+    resp = requests.post('https://api.kroger.com/v1/connect/oauth2/token', headers=headers,data = data)
+    
+    response_data = resp.json()
+    return response_data['access_token']
+def get_me_the_fuck_locations(access_token):
+    header_locations = {"Accept": "application/json","Authorization": "Bearer "+access_token}
+    res = requests.get('https://api.kroger.com/v1/locations?filter.zipCode.near=47408&filter.radiusInMiles=15&filter.chain=Kroger',headers=header_locations)
+    chain_loc = res.json()
+
+    ids = []
+    for i in chain_loc["data"]:
+        ids.append(i['locationId'])
+    return ids
+
+def get_me_the_fuck_cheap(locations,access_token,term,brand):
+    res = ''
+    for id in locations:
+        header_cheap = {"Accept": "application/json","Authorization": "Bearer "+access_token}
+        get_me_the_url="https://api.kroger.com/v1/products?filter.brand="+brand+"&filter.term="+term+"&filter.locationId="+str(id)
+        res = requests.get(get_me_the_url,headers=header_cheap)
+
+    data = res.json()
+    return data["data"][0]["productId"]
+
+def fuckin_cheap_value(prod,access_token,location):
+
+    header_cheap = {"Accept": "application/json","Authorization": "Bearer "+access_token}
+    get_me_the_url="https://api.kroger.com/v1/products/"+str(prod)+"?filter.locationId="+str(location)
+
+    res = requests.get(get_me_the_url,headers=header_cheap)
+
+    data = res.json()
+
+    return data["data"]["items"][0]["price"]
+
+access_token=get_me_the_fuck_in()
+locs = get_me_the_fuck_locations(access_token)
+
+productid = get_me_the_fuck_cheap(locs,access_token,'chocolate','Kroger')
+
+
+@app.get('/kroger')
+def list_of_items():
+   item = request.args.get('item')
+   productid = get_me_the_fuck_cheap(locs,access_token,item,'Kroger')
+   return_value = fuckin_cheap_value(productid,access_token,locs[0])
+   return {"programming_languages":list(return_value.values())}
